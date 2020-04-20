f16fd13b60f5558cc25c9b5d5996b86295f91e5b
ottolaakso <otto.laakso910@gmail.com>
2020-04-20 21:56:28 +0300

    1.0

--- functions.py
+++ functions.py
@@ -20,8 +20,7 @@ def pullStockData(symbol, interval):
     
     df = data_ts
     df.reset_index(inplace=True)
-    print(df)
-
+    
     return df
 
 def graphData(symbol, interval):
@@ -30,30 +29,29 @@ def graphData(symbol, interval):
     
     fig = plt.figure()
     ax1 = plt.subplot2grid((5,4), (0,0), rowspan=4, colspan=4)
-    ax1.plot(df["date", "1. open"])
-    ax1.plot(df["date", "2. high"])
-    ax1.plot(df["date", "3. low"])
-    ax1.plot(df["date", "4. close"])
+    ax1.plot(df["date"], df["1. open"])
+    ax1.plot(df["date"], df["2. high"])
+    ax1.plot(df["date"], df["3. low"])
+    ax1.plot(df["date"], df["4. close"])
     ax1.grid(alpha=.4, linestyle="dashed")
-    ax1.xaxis.set_major_locator(mticker.MaxNLocator(5))
+    ax1.xaxis.set_major_locator(mticker.MaxNLocator(10))
     ax1.xaxis.set_major_formatter(mdates.DateFormatter("%H: %M"))    
     plt.title(symbol+" Price Action")
     plt.xlabel("Date")
     plt.ylabel("Price")
     
     ax2 = plt.subplot2grid((5,4 ), (4,0), sharex=ax1, rowspan=1, colspan=4)
-    ax2.xaxis.set_major_locator(mticker.MaxNLocator(8))
+    ax2.xaxis.set_major_locator(mticker.MaxNLocator(10))
+    ax2.plot(df["date"], df["5. volume"])
     ax2.xaxis.set_major_formatter(mdates.DateFormatter("%H: %M"))    
-    ax2.plot(df["date", "5. volume"])
     ax2.axes.yaxis.set_ticklabels([])
     plt.ylabel("Volume")
     plt.xlabel("Time")
     
-    #plt.setp(ax1.get_xsticklabels(), visible=False)
     plt.subplots_adjust(left=.09, bottom=.10, right=.94, top=.94, wspace=.20, hspace=0)
     plt.style.use("dark_background")
     plt.show()
     
 
 pullStockData(symbol, interval)
-#graphData(symbol, interval)
+graphData(symbol, interval)

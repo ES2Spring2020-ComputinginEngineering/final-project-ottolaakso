1843092d686d4571d6daa0e65636f7ae94e8b6ea
ottolaakso <otto.laakso910@gmail.com>
2020-05-04 19:55:34 +0300

    Comments

--- functions.py
+++ functions.py
@@ -10,7 +10,7 @@ from mpl_finance import candlestick_ochl
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators
 
-symbol = "GOOG"
+symbol = "IBM"
 interval = "1min"
 api_key = "FTOFR6JUG1U8MO6Z"
 
@@ -113,59 +113,54 @@ def tradingAlgorithm(candle_array, upper_band, lower_band, sd, rsi, SP, ax, df):
 def graphData(symbol, interval):
 # Parameters: stock symbol and time interval
 # Graphs the candlestick chart, volume, RSI, and bollinger bands
-# Graphs "Sell" and "Buy" signals given by the trading algorithm
+# Graphs "Sell" and "Buy" signals given by the trading algorithm every 60 seconds
 # Returns None
-    fig.clf()
-    SP = 81
-    df = pullStockData(symbol, interval)
-    candle_array = candleArray(df)
-    rsi = relativeStrengthIndex(symbol, interval)
-    lower_band, middle_band, upper_band, sd = bbands(symbol, interval)
-    #Candlestick & Bollinger Bands Graph:
-    ax1 = plt.subplot2grid((6,4), (1,0), rowspan=4, colspan=4)
-    candlestick_ochl(ax1, candle_array[:SP], width=.0003, colorup="g", colordown="r", alpha=1.0)
-    ax1.plot(df["date"][:SP], middle_band[:SP], linewidth=0.7, color="#00ffe8", alpha=.7)
-    ax1.plot(df["date"][:SP], upper_band[:SP], linewidth=0.7, color="#00ffe8", alpha=.9)
-    ax1.plot(df["date"][:SP], lower_band[:SP], linewidth=0.7, color="#00ffe8", alpha=.7)
-    plt.fill_between(df["date"][:SP], upper_band[:SP],lower_band[:SP],facecolor="#00ffe8", alpha=0.2)
-    ax1.xaxis.set_major_locator(mticker.MaxNLocator(10))
-    ax1.xaxis.set_major_formatter(mdates.DateFormatter("%H: %M"))   
-    plt.grid(linestyle="dashed", alpha=.3)
-    plt.ylabel("Price")
-    #Volume Graph:
-    ax2 = plt.subplot2grid((6,4), (5,0), sharex=ax1, rowspan=1, colspan=4)
-    ax2.bar(df["date"][:SP], df["5. volume"][:SP], width=0.0003, alpha=.8)   
-    ax2.fill_between(df["date"][:SP], df["5. volume"][:SP], facecolor = "#00ffe8",alpha=.4)
-    ax2.xaxis.set_major_formatter(mdates.DateFormatter("%H: %M")) 
-    ax2.axes.yaxis.set_ticklabels([])
-    plt.xlabel("Time")
-    plt.ylabel("Volume")
-    #RSI Graph:
-    ax0 = plt.subplot2grid((6,4), (0,0), sharex=ax1, rowspan=1, colspan=4)
-    ax0.plot(rsi["date"][-SP:], rsi["RSI"][-SP:], color="#00ffe8", alpha=.8)
-    plt.setp(ax0.get_xticklabels(), visible=False)
-    ax0.set_ylim(20,80)
-    ax0.set_yticks([30, 70])
-    ax0.axhline(70, color="red", alpha=.8, linestyle="dashed")
-    ax0.axhline(30, color="green", alpha=.8, linestyle="dashed")
-    plt.title(symbol + " Price Action") 
-    plt.ylabel("RSI") 
-    # Trading Algorithm
-    ax = ax1
-    tradingAlgorithm(candle_array, upper_band, lower_band, sd, rsi, SP, ax, df)
+    while True:     
+        fig.clf()
+        SP = 81
+        df = pullStockData(symbol, interval)
+        candle_array = candleArray(df)
+        rsi = relativeStrengthIndex(symbol, interval)
+        lower_band, middle_band, upper_band, sd = bbands(symbol, interval)
+        #Candlestick & Bollinger Bands Graph:
+        ax1 = plt.subplot2grid((6,4), (1,0), rowspan=4, colspan=4)
+        candlestick_ochl(ax1, candle_array[:SP], width=.0003, colorup="g", colordown="r", alpha=1.0)
+        ax1.plot(df["date"][:SP], middle_band[:SP], linewidth=0.7, color="#00ffe8", alpha=.7)
+        ax1.plot(df["date"][:SP], upper_band[:SP], linewidth=0.7, color="#00ffe8", alpha=.9)
+        ax1.plot(df["date"][:SP], lower_band[:SP], linewidth=0.7, color="#00ffe8", alpha=.7)
+        plt.fill_between(df["date"][:SP], upper_band[:SP],lower_band[:SP],facecolor="#00ffe8", alpha=0.2)
+        ax1.xaxis.set_major_locator(mticker.MaxNLocator(10))
+        ax1.xaxis.set_major_formatter(mdates.DateFormatter("%H: %M"))   
+        plt.grid(linestyle="dashed", alpha=.3)
+        plt.ylabel("Price")
+        #Volume Graph:
+        ax2 = plt.subplot2grid((6,4), (5,0), sharex=ax1, rowspan=1, colspan=4)
+        ax2.bar(df["date"][:SP], df["5. volume"][:SP], width=0.0003, alpha=.8)   
+        ax2.fill_between(df["date"][:SP], df["5. volume"][:SP], facecolor = "#00ffe8",alpha=.4)
+        ax2.xaxis.set_major_formatter(mdates.DateFormatter("%H: %M")) 
+        ax2.axes.yaxis.set_ticklabels([])
+        plt.xlabel("Time")
+        plt.ylabel("Volume")
+        #RSI Graph:
+        ax0 = plt.subplot2grid((6,4), (0,0), sharex=ax1, rowspan=1, colspan=4)
+        ax0.plot(rsi["date"][-SP:], rsi["RSI"][-SP:], color="#00ffe8", alpha=.8)
+        plt.setp(ax0.get_xticklabels(), visible=False)
+        ax0.set_ylim(20,80)
+        ax0.set_yticks([30, 70])
+        ax0.axhline(70, color="red", alpha=.8, linestyle="dashed")
+        ax0.axhline(30, color="green", alpha=.8, linestyle="dashed")
+        plt.title(symbol + " Price Action") 
+        plt.ylabel("RSI") 
+        # Trading Algorithm
+        ax = ax1
+        tradingAlgorithm(candle_array, upper_band, lower_band, sd, rsi, SP, ax, df)
+        plt.subplots_adjust(left=.09, bottom=.10, right=.94, top=.94, wspace=.20, hspace=0)
+        plt.style.use("dark_background")
+        plt.draw()
+        plt.pause(60)
+        
+graphData(symbol, interval)
     
-    plt.subplots_adjust(left=.09, bottom=.10, right=.94, top=.94, wspace=.20, hspace=0)
-    plt.style.use("dark_background")
-    return
-
-def liveData(symbol, interval, fig):
-# Parameters: stock symbol, time interval, plot figure
-# Runs graphData once every minute
-# Returns None
     
-    data = graphData(symbol, interval)
-    animation.FuncAnimation(fig, data, interval=2000)
     
-    return
-
-liveData(symbol, interval, fig) 
+    
